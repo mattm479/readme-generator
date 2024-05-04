d7dc82cfa1d75c6c9f8a02157dec94e76499ff7d
Matthew Miskov <mattm479@gmail.com>
2024-05-04 16:03:16 -0400

    Added package imports and comments for functions

---
+++ index.js
@@ -0,0 +1,16 @@
+// Package imports
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// Array of questions to ask user to populate README
+const questions = [];
+
+// Function call to write README information to specified file
+function writeToFile(fileName, data) {}
+
+// Function call to prompt user for README information
+function init() {}
+
+// Function call to initialize app
+init();
